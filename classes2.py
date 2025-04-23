8a9e3f028ce4e693e52727b9e75f4ab1ada1f247
Salvador <salvadorriosmanzanales@gmail.com>
2025-04-22 23:30:19 -0600

    clases poo

---
+++ classes2.py
@@ -0,0 +1,13 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+
+    def add_passengers(self, name):
+        self.passengers.append(name)
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
